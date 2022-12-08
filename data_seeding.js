220015ae2e2e2b17dbda0020ee16408d3232b328
JohnMVan <john.vanhessche@gmail.com>
2022-12-08 09:33:27 -0600

    data_seeding.js script completion

--- data_seeding.js
+++ data_seeding.js
@@ -2,7 +2,7 @@
 	Title: script.js
     Author: J. Vanhessche
     Date: 7 December 2022
-    Description: MongoDB Shell Scripts for the Whatabook customers and books collections.
+    Description: MongoDB Shell Scripts for the WhatAbook customers and books collections.
  */
 
 
@@ -52,94 +52,64 @@ db.createCollection("customers", {
     }}
 })
 
-//Creating an array of book objects
-let books = [
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-    {"title": "","author": "","genre": "","bookId": ""},
-]
+//A collection of book objects
+book0 = {"title": "Night Road","author": "Kristin Hannah","genre": "Thriller","bookId": "b1001"},
+book1 = {"title": "Dark of the Moon","author": "John Sanford","genre": "Thriller","bookId": "b1002"},
+book2 = {"title": "The Investigator","author": "John Sanford","genre": "Thriller","bookId": "b1003"},
+book3 = {"title": "Thinner","author": "Richard Bachman","genre": "Horror","bookId": "b1004"},
+book4 = {"title": "The Shining","author": "Stephen King","genre": "Horror","bookId": "b1005"},
+book5 = {"title": "It","author": "Stephen King","genre": "Horror","bookId": "b1006"},
+book6 = {"title": "The Forgotten 500","author": "Gregory Freeman","genre": "Non-Fiction","bookId": "b1007"},
+book7 = {"title": "Taken For A Ride","author": "Bradley Stertz","genre": "Non-Fiction","bookId": "b1008"},
+book8 = {"title": "Before We We're Yours","author": "Lisa Wingate","genre": "Non-Fiction","bookId": "b1009"},
+book9 = {"title": "20,000 Leagues Under The Sea","author": "Jules Verne","genre": "Fantasy","bookId": "b1010"},
+book10 = {"title": "The War of The Worlds","author": "H.G Wells","genre": "Fantasy","bookId": "b1011"},    
+book11 = {"title": "The Hobbit","author": "J.R.R. Tolkien","genre": "Fantasy","bookId": "b1012"},
+book12 = {"title": "Getting Started With Wordpress","author": "Todd Kelsey","genre": "How-To","bookId": "b1013"},
+book13 = {"title": "Basecamp for Beginners","author": "Todd Kelsey","genre": "How-To","bookId": "b1014"},
+book14 = {"title": "C# Programming","author": "Barbara Doyle","genre": "How-To","bookId": "b1015"},    
 
-//loop through array and insert each book object into db.books
-//confirm each add with console message.
-for (let book in books) {
-    db.books.insertOne(book);
-    console.log(`${book.title} added successfully`)
-} 
+//A collection of insert statements to insert each book object into db.books
+db.books.insertOne(book0)
+db.books.insertOne(book1)
+db.books.insertOne(book2)
+db.books.insertOne(book3)
+db.books.insertOne(book4)
+db.books.insertOne(book5)
+db.books.insertOne(book6)
+db.books.insertOne(book7)
+db.books.insertOne(book8)
+db.books.insertOne(book9)
+db.books.insertOne(book10)
+db.books.insertOne(book11)
+db.books.insertOne(book12)
+db.books.insertOne(book13)
+db.books.insertOne(book14)
 
-//Creating an array of customers
-let customers = [
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-    {"firstName": "","lastName": "","customerId": "","wishlist": [{"title": "","author": "","genre": "","bookId": ""},]},
-]
+//A collection of customer objects
+customer0 = {"firstName": "Bonnie","lastName": "Swanson","customerId": "c1001","wishlist": []},
+customer1 = {"firstName": "Peter","lastName": "Griffin","customerId": "c1002","wishlist": []},
+customer2 = {"firstName": "Lois","lastName": "Griffin","customerId": "c1003","wishlist": []},
+customer3 = {"firstName": "Joe","lastName": "Swanson","customerId": "c1004","wishlist": []},
+customer4 = {"firstName": "Meg","lastName": "Griffin","customerId": "c1005","wishlist": []},
+customer5 = {"firstName": "Cleveland","lastName": "Brown","customerId": "c1006","wishlist": []},
+customer6 = {"firstName": "Evil","lastName": "Monkey","customerId": "c1007","wishlist": []},
+customer7 = {"firstName": "Neil","lastName": "Goldman","customerId": "c1008","wishlist": []},
+customer8 = {"firstName": "Loretta","lastName": "Brown","customerId": "c1009","wishlist": []},
+customer9 = {"firstName": "Glenn","lastName": "Quagmire","customerId": "c1010","wishlist": []},
 
-//loop through customer array and insert each customer object into db.customers
-//confirm each add with a console message.
-for (let customer in customers) {
-    db.customers.insertOne(customer);
-    console.log(`${customer.lastName} added successfully`)
-}
-
-
-
-
-
+//A collection of insert statements to insert each customer object into db.customers
+db.customers.insertOne(customer0)
+db.customers.insertOne(customer1)
+db.customers.insertOne(customer2)
+db.customers.insertOne(customer3)
+db.customers.insertOne(customer4)
+db.customers.insertOne(customer5)
+db.customers.insertOne(customer6)
+db.customers.insertOne(customer7)
+db.customers.insertOne(customer8)
+db.customers.insertOne(customer9)
 
 
-
-
-
-
-//Deprecated code, for example only
-//books
-// book0 = {"title": "","author": "","genre": "","bookId": ""}
-// book1 = {"title": "","author": "","genre": "","bookId": ""}
-// book2 = {"title": "","author": "","genre": "","bookId": ""}
-// book3 = {"title": "","author": "","genre": "","bookId": ""}
-// book4 = {"title": "","author": "","genre": "","bookId": ""}
-// book5 = {"title": "","author": "","genre": "","bookId": ""}
-// book6 = {"title": "","author": "","genre": "","bookId": ""}
-// book7 = {"title": "","author": "","genre": "","bookId": ""}
-// book8 = {"title": "","author": "","genre": "","bookId": ""}
-// book9 = {"title": "","author": "","genre": "","bookId": ""}
-// book10 = {"title": "","author": "","genre": "","bookId": ""}
-// book11 = {"title": "","author": "","genre": "","bookId": ""}
-// book12 = {"title": "","author": "","genre": "","bookId": ""}
-// book13 = {"title": "","author": "","genre": "","bookId": ""}
-// book14 = {"title": "","author": "","genre": "","bookId": ""}
-// db.books.insertOne(book0)
-// db.books.insertOne(book1)
-// db.books.insertOne(book2)
-// db.books.insertOne(book3)
-// db.books.insertOne(book4)
-// db.books.insertOne(book5)
-// db.books.insertOne(book6)
-// db.books.insertOne(book7)
-// db.books.insertOne(book8)
-// db.books.insertOne(book9)
-// db.books.insertOne(book10)
-// db.books.insertOne(book11)
-// db.books.insertOne(book12)
-// db.books.insertOne(book13)
-// db.books.insertOne(book14)
 
 
